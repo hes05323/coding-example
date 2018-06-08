2c9606bf844b37d052d97b9f7c875e08c170dcad
hes05323 <hes05323@gmail.com>
2018-06-09 00:10:17 +0900

    강의에서 나온 해법을 주석으로 추가함

--- 06-multiplication-table.py
+++ 06-multiplication-table.py
@@ -1,7 +1,7 @@
 num_a=input("몇 단을 출력 하시겠습니까? ")
 
-while int(num_a)>9:
-    print("1~9단까지만 입력해주십시오")
+while int(num_a)>9 or int(num_a)<2:
+    print("2~9단까지만 입력해주십시오")
     num_a=input("몇 단을 출력 하시겠습니끄아악??? ")
 
 num_b=1
@@ -10,3 +10,12 @@ while num_b<10:
     num_c=int(num_a)*int(num_b)
     print(num_a, "*", num_b, "=", int(num_c))
     num_b=int(num_b)+1
+
+
+
+
+## 구구단 만들기 선생님Ver.
+# dan=int(input("몇 단을 출력 하시겠습니까?"))
+# for num in range(1,10):
+#     print("{} * {} = {}".format(dan, num, dan*num))
+    
