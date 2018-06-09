2cf4498a217ee75b31b4d257fe23893025568723
hes05323 <hes05323@gmail.com>
2018-06-09 09:48:19 +0900

    힌트 받은 내용을 합쳐서 나름대 최적화해봄

--- 06-multiplication-table.py
+++ 06-multiplication-table.py
@@ -1,15 +1,15 @@
-num_a=input("몇 단을 출력 하시겠습니까? ")
-
-while int(num_a)>9 or int(num_a)<2:
-    print("2~9단까지만 입력해주십시오")
-    num_a=input("몇 단을 출력 하시겠습니끄아악??? ")
-
-num_b=1
-
-while num_b<10:
-    num_c=int(num_a)*int(num_b)
-    print(num_a, "*", num_b, "=", int(num_c))
-    num_b=int(num_b)+1
+# num_a=input("몇 단을 출력 하시겠습니까? ")
+#
+# while int(num_a)>9 or int(num_a)<2:
+#     print("2~9단까지만 입력해주십시오")
+#     num_a=input("몇 단을 출력 하시겠습니끄아악??? ")
+#
+# num_b=1
+#
+# while num_b<10:
+#     num_c=int(num_a)*int(num_b)
+#     print(num_a, "*", num_b, "=", int(num_c))
+#     num_b=int(num_b)+1
 
 
 
@@ -21,3 +21,13 @@ while num_b<10:
 #         print("{} * {} = {}".format(dan, num, dan*num))
 # else:
 #     print("2에서 9사이의 숫자를 넣어주세요")
+
+
+num_a=int(input("몇 단을 출력 하시겠습니까? "))
+
+while num_a>9 or num_a<2:
+    print("2~9단까지만 입력해주십시오")
+    num_a=int(input("몇 단을 출력 하시겠습니끄아악??? "))
+
+for num_b in range(1,10):
+    print("{} * {} = {}".format(num_a,num_b,num_a*num_b))
